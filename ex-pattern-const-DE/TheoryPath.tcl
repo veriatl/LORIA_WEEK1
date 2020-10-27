9fff474736d14534a4107dc79d504a4b500e72ff
Zheng Cheng <cz0590@gmail.com>
2020-10-27 15:11:29 +0100

    update theory

--- ex-pattern-const-DE/TheoryPath.tcl
+++ ex-pattern-const-DE/TheoryPath.tcl
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <org.eventb.theory.core.scTheoryLanguageRoot org.eventb.core.accurate="true" org.eventb.core.configuration="org.eventb.theory.core.tul" org.eventb.core.source="/ex-pattern-const-DE/TheoryPath.tul">
-<org.eventb.theory.core.scAvailableTheoryProject name="'" org.eventb.core.source="/ex-pattern-const-DE/TheoryPath.tul|org.eventb.theory.core.theoryLanguageRoot#TheoryPath|org.eventb.theory.core.availableTheoryProject#'" org.eventb.theory.core.availableTheoryProject="/theory-axiom-reals">
-<org.eventb.theory.core.scAvailableTheory name="THYPH_0" org.eventb.core.source="/ex-pattern-const-DE/TheoryPath.tul|org.eventb.theory.core.theoryLanguageRoot#TheoryPath|org.eventb.theory.core.availableTheoryProject#'|org.eventb.theory.core.availableTheory#(" org.eventb.theory.core.availableTheory="/theory-axiom-reals/Real.dtf|org.eventb.theory.core.deployedTheoryRoot#Real"/>
+<org.eventb.theory.core.scAvailableTheoryProject name="'" org.eventb.core.source="/ex-pattern-const-DE/TheoryPath.tul|org.eventb.theory.core.theoryLanguageRoot#TheoryPath|org.eventb.theory.core.availableTheoryProject#'" org.eventb.theory.core.availableTheoryProject="/theory-axiom-real">
+<org.eventb.theory.core.scAvailableTheory name="THYPH_0" org.eventb.core.source="/ex-pattern-const-DE/TheoryPath.tul|org.eventb.theory.core.theoryLanguageRoot#TheoryPath|org.eventb.theory.core.availableTheoryProject#'|org.eventb.theory.core.availableTheory#(" org.eventb.theory.core.availableTheory="/theory-axiom-real/Real.dtf|org.eventb.theory.core.deployedTheoryRoot#Real"/>
 </org.eventb.theory.core.scAvailableTheoryProject>
 </org.eventb.theory.core.scTheoryLanguageRoot>
